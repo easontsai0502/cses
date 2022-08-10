d984a5807a23342279810089482699f49d1d722a
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-10 08:29:11 +0800

    1668.cpp

---
+++ 1668.cpp
@@ -0,0 +1,17 @@
+/*
+[Q]https://cses.fi/problemset/task/1084
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define ulli unsigned long long int
+#define lli long long int
+int main(){
+	ulli n,m;
+	cin>>n>>m;
+	return 0;
+}
+
+/*
+*/
