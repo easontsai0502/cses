0c267247131aa4d4f976f98d45293d6d5152074e
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-10 09:03:05 +0800

    1668

--- 1668.cpp
+++ 1668.cpp
@@ -12,16 +12,26 @@ using namespace std;
 	int team[200000];
 void solve(int x,c){
 	team[x]=c;
+	void re=true;
 	for(int i:friendlist[x]){
-		/*確認這一組裡面有沒有friend*/
-		if(team[i]==team[x]){
-			return false;
-		}
-		/*若有朋友沒有分到組*/
-		if(team[i]==0){
-
+		if(re){
+			/*確認這一組裡面有沒有friend*/
+			if(team[i]==team[x]){
+				return false;
+			}
+			/*若有朋友沒有分到組，將朋友分到另一組(不然for迴圈只會把大家分到1裡面)(確保朋友在不同組)*/
+			if(team[i]==0){
+				if(c==1){
+					re=solve(i,2);
+				}else if(c==2){
+					re=solve(i,1);
+				}
+			}
+		}else{
+			return re;
 		}
 	}
+	return re;
 }
 int main(){
 	ulli n,m;
@@ -32,10 +42,13 @@ int main(){
 	while(m--){
 		ulli a,b;
 		cin>>a>>b;
+		a--;
+		b--;
 		friendlist[a].push_back(b);
 		friendlist[b].push_back(a);
 	}
 	void cansolve=true;
+	/*for迴圈的用意：確保大家都有組別*/
 	for(int i=0;i<n && cansolve;i++){
 		if(team[n]){continue;}
 		else{
