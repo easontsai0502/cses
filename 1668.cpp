3e408c3a04622980ab0ae82060b917eee7819b6b
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-10 08:38:39 +0800

    1668

--- 1668.cpp
+++ 1668.cpp
@@ -4,12 +4,30 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 using namespace std;
 #define ulli unsigned long long int
 #define lli long long int
 int main(){
 	ulli n,m;
 	cin>>n>>m;
+	vector<int> friendlist[n];
+	int team[n];
+	for(int i=0;i<n;i++){
+		team[n]=0;
+	}
+	while(m--){
+		ulli a,b;
+		cin>>a>>b;
+		friendlist[a].push_back(b);
+		friendlist[b].push_back(a);
+	}
+	for(int i=0;i<n;i++){
+		if(team[n]){continue;}
+		else{
+			
+		}
+	}
 	return 0;
 }
 
