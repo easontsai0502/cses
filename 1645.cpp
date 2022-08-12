301b671c2098fc01b19003a36fef268700534231
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-12 11:42:34 +0800

    1645

---
+++ 1645.cpp
@@ -0,0 +1,29 @@
+/*
+[Q]https://cses.fi/problemset/task/1645
+[]
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<stack>
+using namespace std;
+#define ulli unsigned long long int
+#define lli long long ulli
+ulli main(){
+    ulli n;
+    cin>>n;
+    ulli x[n];
+    for(ulli i=0;i<n;i++){
+        cin>>x[i];
+    }
+    stack<ulli> st;
+    for(ulli i=0;i<n;i++){
+		
+    }
+	return 0;
+}
+/*
+*/
