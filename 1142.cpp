4abc06632ecee6f7bdef13c86cee07e66e408c32
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-19 12:12:06 +0800

    1142

---
+++ 1142.cpp
@@ -0,0 +1,40 @@
+/*
+[Q]https://cses.fi/problemset/task/1142
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+
+/*struct*/
+
+/*num*/
+
+/*fn*/
+
+/*main*/
+int main(){
+    return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
