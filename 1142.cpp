5639f88fb5c99167be35f18f88738c6636506f85
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-19 13:45:40 +0800

    a

--- 1142.cpp
+++ 1142.cpp
@@ -5,11 +5,16 @@
 
 /*include*/
 #include<iostream>
-#include<vector>
 #include<algorithm>
 #include<cmath>
 #include<string>
 #include<sstream>
+#include<vector>
+#include<struct>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
 
 /*using namespace*/
 using namespace std;
@@ -17,6 +22,11 @@ using namespace std;
 /*define type*/
 #define ulli unsigned long long int
 #define lli long long int
+#define pii pair<int,int>
+#define plli pair<lli,lli>
+#define pulli pair<ulli,ulli>
+#define INT int
+#define UINT unsigned INT
 
 /*struct*/
 
@@ -26,6 +36,24 @@ using namespace std;
 
 /*main*/
 int main(){
+    UINT n;
+    cin>>n;
+    UINT k[n];
+    UINT maxk=0;
+    for(int i=0;i<n;i++)cin>>k[i],maxk=max(maxk,k[i]);
+    map<UINT,UINT> highpoint;/*紀錄每個高度的竿子的位置*/
+    UINT ans=0;
+    for(int i=0;i<n;i++){
+        if(i){
+            if(!(highpoint.count(k[i]))){
+                highpoint[k[i]]=i;
+            }
+            ans=max((i-highpoint[k[i]]+1)*k[i],ans);
+        }else{
+            ans=k[i];
+            highpoint[k[i]]=i;
+        }
+    }
     return 0;
 }
 
