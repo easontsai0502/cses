7fb63620e97fb45f49a6ed9f78ca3482ea840143
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-19 12:16:02 +0800

    basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,45 @@
+/*
+[Q]
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<struct>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+
+/*struct*/
+
+/*num*/
+
+/*fn*/
+
+/*main*/
+int main(){
+    return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
